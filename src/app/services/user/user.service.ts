09f09c485a04cb5ef911a57e9ebeeae77b0e687b
harshrajsinghsengar <harshrajsingh094@gmail.com>
2021-09-06 19:37:28 +0530

    commit

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,7 +1,8 @@
 import { User } from './../../models/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,15 +10,39 @@ import { map } from 'rxjs/operators';
 export class UserService {
   private userSignupUrl = 'http://localhost/api/users/signup';
   private userLoginUrl = 'http://localhost/api/users/login';
+  private isAdminUrl = 'http://localhost/api/users/is-admin';
+  private _loginObservable: BehaviorSubject<Object>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._loginObservable = new BehaviorSubject({});
+  }
+
+  public get loginObservable() {
+    return this._loginObservable;
+  }
 
   private saveTokenToLocalStorage(token: string) {
     localStorage.setItem('token', 'Bearer ' + token);
   }
 
+  logout() {
+    localStorage.removeItem('token');
+    this._loginObservable.next({});
+  }
+
   getToken() {
     return localStorage.getItem('token') ? localStorage.getItem('token') : '';
+  }
+
+  isAdmin() {
+    let headers = new HttpHeaders({
+      authorization: this.getToken(),
+    });
+    return this.http.get(this.isAdminUrl, { headers }).pipe(
+      map((result) => {
+        return <boolean>result;
+      })
+    );
   }
 
   signup(user: User) {
@@ -32,6 +57,7 @@ export class UserService {
     return this.http.post(this.userLoginUrl, credentials).pipe(
       map((result: loginResponse) => {
         this.saveTokenToLocalStorage(result.token);
+        this._loginObservable.next({});
         return result;
       })
     );
