8598324745610bf43276e45d3ba9c2e0111d6e67
harshrajsinghsengar <harshrajsingh094@gmail.com>
2021-09-10 23:04:35 +0530

    commit

--- src/app/components/admin/admin-orders/admin-orders.component.ts
+++ src/app/components/admin/admin-orders/admin-orders.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/models/order';
+import { OrderService } from 'src/app/services/order/order.service';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
-  styleUrls: ['./admin-orders.component.css']
+  styleUrls: ['./admin-orders.component.css'],
 })
 export class AdminOrdersComponent implements OnInit {
+  modelRef: BsModalRef;
+  selectedOrder: Order;
 
-  constructor() { }
+  orders$: Observable<Order[]>;
+  constructor(
+    private orderService: OrderService,
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit(): void {
+    this.collectOrders();
   }
 
+  collectOrders() {
+    this.orders$ = this.orderService.getAdminOrders();
+  }
+
+  changeStatus(status: string, order: Order) {
+    this.orderService.changeStatus({ status: status }, order._id).subscribe({
+      next: (result) => {
+        console.log(result);
+        order.status = status;
+      },
+    });
+  }
+
+  showDetails(order: Order, table) {
+    this.selectedOrder = order;
+    this.modelRef = this.modalService.show(table, { class: 'modal-lg' });
+  }
+
+  close() {
+    this.modelRef.hide();
+  }
 }
